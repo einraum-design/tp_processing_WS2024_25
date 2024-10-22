2ecdaf66cfb16eb8682ccd97dc06cf614609d15c
Manuel Michel <mail@manuelmichel.de>
2024-10-22 21:43:34 +0200

    kursmaterial vom 18.10.2024

--- docs/TP1/sketch.js
+++ docs/TP1/sketch.js
@@ -1,55 +1,63 @@
-/* ich
-  bin
-  ein 
-  mehrzeiliger 
-  kommentar
-  */
-
-
 function setup() {
-  createCanvas(400, 400);
+  // comment - its ignored by the browser
+  
+  /* multi
+     line
+     comment
+  */
   
   // Winkel in Grad statt Bogenmaß angeben:
+  // work with degrees (0-360°) instead of radians (0-TWO_PI)
   angleMode(DEGREES);
   
-  // Farben 0 - 255 --> R, G, B, (A)
-  background(0, 0, 255);
+  // crates a canvas in measers width , height in px
+  createCanvas ( 400,  300 );
+  
+  // 1 Paramter -> greyValue, 3 parameters red, green, blue
+  background(200);
+  
   
   // Füllfarbe festlegen
   // Farben 0 - 255 --> R, G, B, (A)
   // Farben 0 - 255 --> Grauwert, (A)
-  fill(255, 150, 0);
+  fill(255, 0, 0);
+  
+  // rectMode(CENTER / CORNER)
+  // Referenzpunkt, von wo das Rechteck aus positioniert wird
+  rectMode(CENTER);
+  
+  // rect(x, y, w, h);
+  rect(150, 100, 100, 60);
   
   
+  // ellipseMode(CENTER / CORNER);
+  
+  // fill(red, gree, blue, alpha)
+  fill(0, 0, 255, 100);
+  
+  // ellipse(x, y, w, h);
+  ellipse(150, 100, 100, 60);
+  
+
   // Referenzpunkt, von wo der Kreis aus positioniert wird
   // CENTER / CORNER
   ellipseMode(CORNER);
-  // ellipse(X-POS, Y-POS, WIDTH, HEIGHT);
-  ellipse(200, 150, 100, 100);
   
+  // noFill() -> no fill color
+  noFill();
+  stroke(0, 255, 0);
+  strokeWeight(6);
+  ellipse(150, 100, 100, 60);
   
-  fill(255, 100);
-  stroke(255);
-  strokeWeight(5);
+  // fill(greyValue) --> red green and blue are equal
+  fill(120);
+  ellipse(250, 100, 100, 60);
   
-  // Referenzpunkt, von wo der Rechteck aus positioniert wird
-  // CENTER / CORNER
-  rectMode(CENTER);
-  // rect(X-POS, Y-POS, WIDTH, HEIGHT);
-  rect(200, 150, 100, 100);
-  
-  
-  noStroke();
-  triangle(50,50, 150, 100, 100, 200);
-  
+  line(20, 200, 35, 25);
   
   fill(255, 255, 0);
-  arc(100, 300, 100, 100, 20, 360-20 , PIE);
-  
-  stroke(0);
-  strokeWeight(6);
-  point(150, 330);
-  
+  // arc(x, y, w, h, startangle, stopangle, paintMode)
+  arc(200, 200, 100, 100, 45, 360-45, PIE)
 }
 
 function draw() {
