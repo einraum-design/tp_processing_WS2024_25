bf6a0a5ec944249edfd380861a8bed9a52ed4283
Manuel Michel <mail@manuelmichel.de>
2024-11-22 13:31:11 +0100

    Kurs vom 22.11.2024

---
+++ docs/TP5_2/sketch.js
@@ -0,0 +1,57 @@
+
+
+function setup() {
+  createCanvas(400, 400);
+  angleMode(DEGREES);
+}
+
+function draw() {
+  background(220);
+
+  translate(width/2, height/2);
+
+  // second() -> int 0 - 60
+  let secondRotation = map(second(), 0, 60, 0, 360);
+
+  rotate(secondRotation);
+  stroke(255, 0, 0);
+  strokeWeight(1);
+  line(0, 0, 0, -180);
+
+  // Zurücksetzen der Rotation vom Sekundenzeiger
+  // Option 1:
+  // resetMatrix();
+  // translate(width/2, height/2);
+
+  // Option 2:
+  // zurückdrehen der vorherigen Rotation
+  rotate(-secondRotation);
+
+  
+  // Option 3
+  // aktuellen Zustand der Transformationen zwischenspeichern
+  push();
+
+  // minute() -> int 0 - 60
+  let minuteRotation = map(minute(), 0, 60, 0, 360);
+
+  rotate(minuteRotation);
+  stroke(0);
+  strokeWeight(3);
+  line(0, 0, 0, -190);
+
+  pop();
+
+  push();
+  // hour() -> int 0 - 24
+  let hourRotation = map(minute(), 0, 12, 0, 360);
+
+  rotate(hourRotation);
+  stroke(0);
+  strokeWeight(5);
+  line(0, 0, 0, -150);
+
+
+  pop();
+
+}
